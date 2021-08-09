39125f9994a6c6cf281555150302af22040cbbab
Rishabh Pandey <rishabh.pandey995@gmail.com>
2021-08-10 04:04:29 +0530

    Fix styles

--- source/_components/image.blade.php
+++ source/_components/image.blade.php
@@ -1,18 +1,17 @@
-    <section class="pt-6 pt-md-8">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-12 {{ isset($wide) && $wide ? '' : 'col-md-10 col-lg-9 col-xl-8' }}">
-                    <figure class="figure mb-7">
-                        <img class="figure-img img-fluid rounded" src="{{ $url }}"
-                            alt="{{ $alt ?? 'Post Image' }}">
+<section class="pt-6 pt-md-8">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 {{ isset($wide) && $wide ? '' : 'col-md-10 col-lg-9 col-xl-8' }}">
+                <figure class="figure mb-7 text-center">
+                    <img class="figure-img img-fluid rounded" src="{{ $url }}" alt="{{ $alt ?? 'Post Image' }}">
 
-                        @if (isset($caption))
-                        <figcaption class="figure-caption text-center">
-                            {{ $caption }}
-                        </figcaption>
-                        @endif
-                    </figure>
-                </div>
-            </div> <!-- / .row -->
-        </div> <!-- / .container -->
-    </section>
+                    @if (isset($caption))
+                    <figcaption class="figure-caption text-center">
+                        {{ $caption }}
+                    </figcaption>
+                    @endif
+                </figure>
+            </div>
+        </div> <!-- / .row -->
+    </div> <!-- / .container -->
+</section>
--- source/_components/quote.blade.php
+++ source/_components/quote.blade.php
@@ -1,12 +1,12 @@
 <hr class="hr-md my-7 text-secondary">
 
 <blockquote class="blockquote mb-7">
-  <p class="h2 mb-0 text-center text-primary">
+  <p class="h2 mb-0 text-center">
     {{ $slot }}
   </p>
 
   @if (isset($credit))
-  <p class="h5 mt-4 text-center text-primary">
+  <p class="h4 mt-5 text-center">
     {{ $credit }}
   </p>
   @endif
